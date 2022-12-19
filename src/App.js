52a97d2527e22dbf801e75565535e6b7e3b1ca3d
Mark <markoconnor1@gmail.com>
2022-12-19 14:14:14 +0000

    Add spinner and some css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import ReactPaginate from 'react-paginate';
 import './App.css';
+import Spinner from './components/Spinner';
 
 const PER_PAGE = 10;
 
 function App() {
   const [cityData, setCityData] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
-
+  const [isLoading, setIsLoading] = useState(true);
+  
   async function getData() {
     const response = await fetch('https://datahub.io/core/world-cities/r/world-cities.json');
     const data = await response.json();
+    setIsLoading(false);
     setCityData(data);
   }
   
   useEffect(() => {
     getData();
   }, []);
+  
+  if (isLoading) {
+    return <Spinner message="Getting city data..." />;
+  }
 
   function handlePageClick({ selected: selectedPage }) {
     setCurrentPage(selectedPage);
@@ -43,7 +50,7 @@ function App() {
       <div>
         <h1>World Cities</h1>
       </div>
-      <div>
+      <div className='ui container'>
         <table className='ui green table'>
           <thead>
             <tr>
@@ -57,22 +64,28 @@ function App() {
             {currentPageData}
           </tbody>
         </table>
-        <ReactPaginate
-        previousLabel={"← Previous"}
-        nextLabel={"Next →"}
-        pageCount={pageCount}
-        onPageChange={handlePageClick}
-        containerClassName={"pagination"}
-        previousLinkClassName={"pagination__link"}
-        nextLinkClassName={"pagination__link"}
-        disabledClassName={"pagination__link--disabled"}
-        activeClassName={"pagination__link--active"}
-      />
+        <div className='ui container'>      
+          <ReactPaginate
+            previousLabel={"← Previous"}
+            nextLabel={"Next →"}
+            pageCount={pageCount}
+            onPageChange={handlePageClick}
+            containerClassName={"pagination"}
+            previousLinkClassName={"pagination__link"}
+            nextLinkClassName={"pagination__link"}
+            disabledClassName={"pagination__link--disabled"}
+            activeClassName={"active"}
+          />
+        </div>
       </div>
       </div>
         <div className='eight wide column'>
-          <h1>Dummy Text</h1>
-          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          <div>
+            <h1>Dummy Text</h1>
+          </div>
+          <div className='ui container'>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          </div>
         </div>
     </div>
   );
