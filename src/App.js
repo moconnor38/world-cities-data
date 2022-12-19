bc048bdc5d9855c15ec4ee658eb514b3589015bd
Mark <markoconnor1@gmail.com>
2022-12-19 12:26:14 +0000

    Get data from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+  async function getData() {
+    const response = await fetch('https://datahub.io/core/world-cities/r/world-cities.json');
+    const data = await response.json();
+    console.log(data);
+  }
+  
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div >
       <h1>World Cities</h1>
